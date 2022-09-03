056f588a769ac03ce5442179ded9a3cf0c260bac
Aaron Romero <far_aro@outlook.com>
2022-09-02 23:41:38 -0700

    Agregar archivos de proyecto.

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Calculator.Models;
+
+namespace Calculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly CalculatorContext _db;
+        public CalculatorController(CalculatorContext context)
+        {
+            _db = context;
+        }
+
+    }
+}
